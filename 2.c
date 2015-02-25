80270b72f783470c8f7a23b673ca80bf5b4a2451
Kenner Kliemann <kenner.hp@gmail.com>
2015-02-24 21:34:09 -0300

    Exe 2

---
+++ 2.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define BUFFER_LIMIT 80
+
+void remove_carriage_return_from_string(char *str)  {
+  char *pos;
+
+  if ((pos=strchr(str, '\n')) != NULL)
+    *pos = '\0';
+}
+
+void* reader()  {
+  char *str;
+  char aux[BUFFER_LIMIT];
+
+  printf("Enter a string : ");
+  fgets(aux, BUFFER_LIMIT, stdin);
+  printf("You entered: %s", aux);
+
+  remove_carriage_return_from_string(aux);
+
+  str = malloc(sizeof(char) * strlen(aux));
+
+  strcpy(str, aux);
+
+  return str;
+}
+
+int main(int argc, char const *argv[]) {
+
+  char *str_1 = reader();
+
+  int size_of_string = strlen(str_1);
+
+  printf("String content: %s, and string lenght: %i\n", str_1, size_of_string);
+
+  return 0;
+}
--- Makefile
+++ Makefile
@@ -3,3 +3,6 @@ CFLAGS = -g -Wall -o
 
 1: 1.c
 	$(CC) $(CFLAGS) exe1 1.c
+
+2: 2.c
+	$(CC) $(CFLAGS) exe2 2.c
