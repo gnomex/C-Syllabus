3b306f03e48fe961716b69400f158e4d6d5963a3
Kenner Kliemann <kenner.hp@gmail.com>
2015-02-26 14:42:11 -0300

    Compare two multidimencional arrays for exe 11

--- 11.c
+++ 11.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 
 #define COLUMNS 4
-#define ROWS 3
+#define ROWS 4
 
 int posix_reader(char *str)  {
   int number;
@@ -36,12 +36,33 @@ void show_multi_array_ptr(int *matrix) {
   }
 }
 
+/* @Returns:
+  0 for false
+  1 to true
+*/
+int check_if_win(int *valid, int *to_compare) {
+  int i = 0;
+
+  for (; i < ROWS; i++)  {
+    int j = 0;
+
+    for ( ; j < COLUMNS; j++)  {
+
+      if (to_compare[i*COLUMNS +j] != valid[i*COLUMNS +j])  {
+        return 0;
+      }
+    }
+  }
+  return 1;
+}
+
 int main(int argc, char const *argv[])  {
-  int valid_puzzle[ROWS][COLUMNS] = { {1,2,3,4}, {5,6,7,8}, {9,10,11,0} };
+  int valid_puzzle[ROWS][COLUMNS] = { {1,2,3,4}, {5,6,7,8}, {9,10,11,12}, {13,14,15,0} };
+  int puzzle[ROWS][COLUMNS];
 
-  show_multi_array(valid_puzzle);
+  // show_multi_array(valid_puzzle);
 
-  show_multi_array_ptr(&valid_puzzle);
+  // show_multi_array_ptr(&valid_puzzle);
 
   return 0;
 }
