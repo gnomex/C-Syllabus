27bf2346e87233a9c3de972f7fcdec65879f20fd
Kenner Kliemann <kenner.hp@gmail.com>
2015-03-28 15:37:30 -0300

    Improve the buffer methods

--- FAA/1th_work/src/encrypt.c
+++ FAA/1th_work/src/encrypt.c
@@ -9,15 +9,34 @@ t_buffer* give_me_a_buffer( size_t size )  {
   t_buffer *buffer = NULL;
   char *data = NULL;
 
+  size_t reserved_size = size + 1;
+
   buffer = (struct _Buffer *) malloc (sizeof (struct _Buffer));
   memset( buffer, 0, sizeof (struct _Buffer) );
 
-  data = (char *) malloc (sizeof(char) * size + 1); // Always more than you need, for security reasons
+  data = (char *) malloc (sizeof(char) * reserved_size); // Always more than you need, for security reasons
 
   buffer->data = data;
-  buffer->length = size + 1;
+  buffer->allocated_size = reserved_size;
+  buffer->current_legth = strlen(buffer->data);
 
   return buffer;
+}
+
+/**
+  Assign content data to a buffer and check buffer overflow.
+  @param buffer: the buffer
+  @param data: the content do assign to the buffer
+*/
+void assign_data_to_buffer( t_buffer *buffer, char *data) {
+  size_t data_length = strlen(data);
+
+  if ( data_length > buffer->allocated_size ) {
+    die("You tried to put a bigger array that your buffer can hold");
+  }
+
+  strncpy( buffer->data, data, data_length ); // Avoid buffer overflows
+  buffer->current_legth = data_length;
 }
 
 /**
@@ -33,7 +52,44 @@ void reverse_me(char *p)  {
     *p = *p ^ *q;
 }
 
+// size_t find_the_size_of_text_file( FILE *file ) {
+//   size_t size = 0;
 
+//   fseek(file, 0L, SEEK_END);
+//   size = ftell(file);
+//   fseek(file, 0L, SEEK_SET);
+
+//   return size;
+// }
+
+// void read_the_file( size_t *stream, char *filename ) {
+//   int streamer_size = stream->length;
+//   int approx = 0;
+
+//   FILE *file = NULL;
+
+//   t_buffer *aux = give_me_a_buffer(streamer_size);
+
+//   if (file = fopen(filename,"r")){
+//     approx = find_the_size_of_text_file( file );
+
+//     fread(???)
+
+//     while ( !feof(file) ){
+
+
+//       fread(???);
+//     }
+
+//     fclose(file);
+
+//   }else{
+//     deal_with_errors();
+//   }
+
+//   free(aux);
+
+// }
 // read_from_file -> filename
 //   a buffer
 //   a file
@@ -61,23 +117,27 @@ void reverse_me(char *p)  {
 // decrypt -> buffer, salt
 //   magic
 
-// reverse -> buffer
+void die( char *message ) {
+  printf("[ERROR] %s. {INFO: Use gdb to debbug}\n", message);
+  exit(1);
+}
 
 
 int main(int argc, char const *argv[])  {
 
-  t_buffer *a = give_me_a_buffer(100);
+  t_buffer *a = give_me_a_buffer(10);
 
-  strcpy(a->data, "123456789101112131415");
+  assign_data_to_buffer( a, "1234567891");
 
   printf("Data is: %s\n", a->data);
 
   printf("Length of data is: %d \n", strlen(a->data));
-  printf("Associated length is: %d\n", a->length);
+  printf("Associated length is: %d\n", a->current_legth);
+  printf("Allocated buffer size is: %d\n", a->allocated_size);
 
   reverse_me( a->data );
 
-  printf("Reversed is %s\n", a->data);
+  printf("Reversed is: %s\n", a->data);
 
   free(a);
 
--- FAA/1th_work/src/encrypt.h
+++ FAA/1th_work/src/encrypt.h
@@ -13,14 +13,21 @@
 
 typedef struct _Buffer {
   char *data;
-  size_t length;
+  size_t allocated_size;
+  size_t current_legth;
 } t_buffer;
 
 typedef void (*fptr)(void);
 
 t_buffer* give_me_a_buffer( size_t size );
+void assign_data_to_buffer( t_buffer *buffer, char *data);
+
 void strrev(char *p);
 
+void find_the_size_of_text_file( FILE *file );
+
 // fptr  ptrs[3] = { NULL, another_thing, something };
+
+void die(char *message);
 
 #endif /* !ENCRYPTER */
