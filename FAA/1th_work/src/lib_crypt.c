0296895c40435f357dbb211b735b0fe3e0537db0
Kenner Kliemann <kenner.hp@gmail.com>
2015-04-02 14:47:14 -0300

    The crypter engine :+1:

--- FAA/1th_work/src/lib_crypt.c
+++ FAA/1th_work/src/lib_crypt.c
@@ -1,8 +1,13 @@
 #include "lib_crypt.h"
 
-// int crypt( char *password, char *salt ) {
+int crypt( char *password, char *salt, t_buffer *destination ) {
+  return 1;
+}
 
-// }
+int decrypt( char *password, char *salt, t_buffer *destination ) {
+  return 1;
+}
+
 
 unsigned char rotate_carry_left_of_char( const unsigned char ch,
                                          const unsigned char d )  {
@@ -49,7 +54,7 @@ t_buffer* give_me_a_chunk_from_file( const char *filename,
   */
   t_buffer *buffer = NULL; buffer = give_me_a_buffer( (meta - current) );  // The whole buffer
 
-  if ( file = fopen( filename, "r" ) ) {
+  if ( (file = fopen( filename, "r" )) ) {
     fseek( file, current, SEEK_CUR );
 
     char current;
@@ -77,55 +82,65 @@ t_buffer* give_me_a_chunk_from_file( const char *filename,
 // void partial_reader_the_file( t_buffer *stream, char *filename ) {
 //   int streamer_size = stream->current_legth;
 //   int approx = 0;
-
 //   FILE *file = NULL; // The given FILE
-
 //   t_buffer *aux = give_me_a_buffer(streamer_size);  // The whole buffer
-
 //   if ( file = fopen( filename, "r" ) ) {
 //     approx = find_the_size_of_text_file( file );
-
 //     if ( streamer_size > approx ) die("Action: read the input file, message: The stream is bigger than the file");
-
 //     // printf("File lenght is: %d\n", approx); // Low level debug
-
 //     while ( !feof(file) ) {
 //       if (buffer.is_full) {
 //         sync_and_reset( aux );
 //       }
-
 //       char current = fgetc(file);
 //       append_to_buf(current);
 //     } /* END_FEOF_FILE*/
-
-
 //     fclose(file);
-
 //   } else  {
 //     // deal_with_errors();
 //   }
-
 //   free(aux); // Tnks buffer for your service
 // }
 
-// void partial_writer_to_a_file( t_buffer *stream, char *filename )  {
-//   FILE *file = NULL;  // The given file
+void partial_writer_to_a_file( t_buffer *stream, char *filename )  {
+  FILE *file = NULL;  // The given file
 
-//   if ( file = fopen( filename, "a" ) ) {
+  if ( (file = fopen( filename, "a" )) ) {
 
-//     fputs( stream->data, file );
+    fputs( stream->data, file );
 
-//     fclose( file );
-//   } else  {
-//     // deal_with_errors();
-//   }
-// }
+    fclose( file );
+  } else  {
+    // deal_with_errors();
+  }
+}
 
-// crypt -> buffer, salt
-//   magic
+void crypt_engine( f_descriptors *files,
+                   const t_buffer *password,
+                   int option ) {
+  unsigned int f_counter = 0;
+  unsigned int meta = password->current_legth;
+  int f_eof = 0;
 
-// decrypt -> buffer, salt
-//   magic
+  t_buffer *aux = NULL;
+  t_buffer *crypter = NULL; crypter = give_me_a_buffer( aux->allocated_size );
+
+  while ( !f_eof )  {
+    aux = give_me_a_chunk_from_file( files->input_file , f_counter, meta, &f_eof );
+
+    switch (option) {
+      case ENCRYPT:
+        crypt( password->data, aux->data, crypter);
+        break;
+      case DECRYPT:
+        decrypt( password->data, aux->data, crypter);
+        break;
+    }
+
+    partial_writer_to_a_file( crypter, files->output_file );
+    f_counter += meta;
+  }
+}
 
 /**
   Reverse a char array. Side effects!
@@ -152,4 +167,36 @@ void die( const char *message ) {
   }
 
   exit(1);
+}
+
+
+f_descriptors* give_me_a_f_descriptor( size_t size )  {
+  f_descriptors *buffer = NULL;
+  char *in = NULL;
+  char *out = NULL;
+
+  size_t reserved_size = size + 1;
+
+  buffer = (f_descriptors *) malloc (sizeof (f_descriptors));
+  memset( buffer, 0, sizeof (f_descriptors) );
+
+  in = (char *) malloc (sizeof(char) * reserved_size); // Always more than you need, for security reasons
+  out = (char *) malloc (sizeof(char) * reserved_size); // Always more than you need, for security reasons
+
+  buffer->input_file = in;
+  buffer->output_file = out;
+
+  return buffer;
+}
+
+void assign_fin( f_descriptors *src, const char *filename ) {
+  size_t data_length = strlen(filename);
+
+  strncpy( src->input_file, filename, data_length ); // Avoid buffer overflows
+}
+
+void assign_fout( f_descriptors *src, const char *filename ) {
+  size_t data_length = strlen(filename);
+
+  strncpy( src->output_file, filename, data_length ); // Avoid buffer overflows
 }
--- FAA/1th_work/src/lib_crypt.h
+++ FAA/1th_work/src/lib_crypt.h
@@ -10,6 +10,13 @@
 #define OUTFD 1
 #define CH_HIGHER 255
 #define CH_LOWER 0
+#define ENCRYPT 1
+#define DECRYPT 2
+
+typedef struct {
+  char *input_file;
+  char *output_file;
+} f_descriptors;
 
 // typedef void ( *fptr )( void );
 // typedef unsigned char ( *f_rotate )( unsigned char a, unsigned char b );
@@ -25,6 +32,10 @@ t_buffer* give_me_a_chunk_from_file( const char *filename,
                                      const size_t current,
                                      const size_t meta,
                                      int *c_eof );
+
+void crypt_engine( f_descriptors *files,
+                   const t_buffer *password,
+                   int option );
 
 void die( const char *message );
 
