fe24febc4a5b78baa1f9c2ba88aa881a1d0674cb
Kenner Kliemann <kenner.hp@gmail.com>
2015-06-18 09:21:40 -0300

    Sketch for the structure

--- FAA/2th_work/src/list_iterator.c
+++ FAA/2th_work/src/list_iterator.c
@@ -1,1 +1,18 @@
 #include "list.h"
+
+/*
+ * Allocates a new airports_t. NULL on failure.
+ */
+airports_t *
+new_airport_list()
+{
+  airports_t * self = NULL;
+  if (!(self = (airports_t *) LIST_MALLOC(sizeof(airports_t))))
+    return NULL;
+  self->head  = NULL;
+  self->tail  = NULL;
+  self->free  = NULL;
+  self->match = NULL;
+  self->len   = 0;
+  return self;
+}
--- FAA/2th_work/src/list_nodes.c
+++ FAA/2th_work/src/list_nodes.c
@@ -44,20 +44,3 @@ list_flight_new(const int uid, const char *company_name)
   self->prev  = NULL;
   return self;
 }
-
-/*
- * Allocates a new airports_t. NULL on failure.
- */
-airports_t *
-new_airport_list()
-{
-  airports_t * self = NULL;
-  if (!(self = (airports_t *) LIST_MALLOC(sizeof(airports_t))))
-    return NULL;
-  self->head  = NULL;
-  self->tail  = NULL;
-  self->free  = NULL;
-  self->match = NULL;
-  self->len   = 0;
-  return self;
-}
