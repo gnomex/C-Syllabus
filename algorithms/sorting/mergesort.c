fc45e8f94ef5ee2434bdc8fcfaa089cea03bb394
Kenner Kliemann <kenner.hp@gmail.com>
2017-03-20 23:15:03 -0300

    Merge sort

--- algorithms/sorting/mergesort.c
+++ algorithms/sorting/mergesort.c
@@ -1,75 +1,108 @@
+/* Merge sort in C from https://gist.github.com/mycodeschool/9678029 */
 #include <stdio.h>
+#include <stdlib.h>
 
-#define ARRAY_LENGTH 10
+// #define ARRAY_LENGTH 10
 #define TM_PRINTF(f_, ...) printf((f_), __VA_ARGS__)
 
-void merge(int *array, int a1, int b1, int a2, int b2) {
+// void merge(int *array, int a1, int b1, int a2, int b2) {
+//   int i,j,k;
+//   i = a1;
+//   j = a2;
+//   // TM_PRINTF("merging a1[%d] b1[%d] a2[%d] b2[%d]\n", a1, b1, a2, b2);
+//   printArray("merging: ", array);
+//   for( k = 0; k <= b2; k++){
+//     if (i > b1) {
+//       // TM_PRINTF("replace k[%d] : ak[%d] -> aj[%d] \n", k, array[k], array[j]);
+//       array[k] = array[j++];
+//     } else if (j > b2) {
+//       // TM_PRINTF("replace k[%d] : ak[%d] -> ai[%d] \n", k, array[k], array[i]);
+//       array[k] = array[i++];
+//     }
+//     if (array[i] <= array[j])
+//       array[k] = array[i++];
+//     else
+//       array[k] = array[j++];
+//   }
+// }
+// void merge_sort(int *array, int start, int end) {
+//   int m;
+//   if (start >= end) return;
+//   m = (start + end)/2;
+//   merge_sort(array, start, m);
+//   merge_sort(array, m+1, end);
+//   merge(array, start, m, m+1, end);
+// }
+
+
+
+// Function to Merge Arrays L and R into A.
+// lefCount = number of elements in L
+// rightCount = number of elements in R.
+void Merge(int *A, int *L, int leftCount, int *R, int rightCount) {
   int i,j,k;
 
-  i = a1;
-  j = a2;
+  // i - to mark the index of left aubarray (L)
+  // j - to mark the index of right sub-raay (R)
+  // k - to mark the index of merged subarray (A)
+  i = 0; j = 0; k =0;
 
-  TM_PRINTF("merging a1[%d] b1[%d] a2[%d] b2[%d]\n", a1, b1, a2, b2);
-  printArray("merging: ", array);
-
-  for( k = 0; k <= b2; k++){
-
-    if (i > b1) {
-      TM_PRINTF("replace k[%d] : ak[%d] -> aj[%d] \n", k, array[k], array[j]);
-      array[k] = array[j++];
-    }
-    else if (j > b2) {
-      TM_PRINTF("replace k[%d] : ak[%d] -> ai[%d] \n", k, array[k], array[i]);
-      array[k] = array[i++];
-    }
-
-    if (array[i] <= array[j])
-      array[k] = array[i++];
-    else
-      array[k] = array[j++];
+  while(i<leftCount && j< rightCount) {
+    if(L[i]  < R[j]) A[k++] = L[i++];
+    else A[k++] = R[j++];
   }
+  while(i < leftCount) A[k++] = L[i++];
+  while(j < rightCount) A[k++] = R[j++];
 }
 
-void merge_sort(int *array, int a, int b) {
-  int m;
+// Recursive function to sort an array of integers.
+void MergeSort(int *A, int n) {
+  int mid, i, *L, *R;
 
-  if (a >= b) {
-    array[a] = array[b];
-    return;
-  }
+  if( n < 2 ) return; // base condition. If the array has less than two element, do nothing.
 
-  m = (a+b)/2;
+  mid = n / 2;  // find the mid index.
 
-  TM_PRINTF("M %d \n", m);
+  // create left and right subarrays
+  // mid elements (from index 0 till mid-1) should be part of left sub-array
+  // and (n-mid) elements (from mid to n-1) will be part of right sub-array
+  L = (int*) malloc( mid * sizeof(int) );
+  R = (int*) malloc( (n - mid) * sizeof(int) );
 
-  merge_sort(array, a, m);
-  merge_sort(array, m+1, b);
+  for( i = 0; i<mid; i++ ) L[i] = A[i]; // creating left subarray
+  for( i = mid; i<n; i++ ) R[i-mid] = A[i]; // creating right subarray
 
-  merge(array, a, m, m+1, b);
+  MergeSort(L,mid);  // sorting the left subarray
+  MergeSort(R,n-mid);  // sorting the right subarray
+  Merge(A,L,mid,R,n-mid);  // Merging L and R into A as sorted list.
+
+  free(L);
+  free(R);
 }
 
-
-void printArray(char *str, int* array)  {
+void printArray(char *str, int* array, int size)  {
   int i;
 
   if (str != NULL) TM_PRINTF("%s", str);
 
-  for (i = 0; i < ARRAY_LENGTH; i++) {
-    TM_PRINTF("%d ", array[i]);
-  }
+  for (i = 0; i < size; i++) TM_PRINTF("%d ", array[i]);
 
   TM_PRINTF("\n", NULL);
 }
 
 int main() {
 
-  int a[ARRAY_LENGTH] = {9, 2, 1, 4, 8, 6, 7, 2, 3, 5};
+  int A[] = {9, 2, 1, 4, 8, 6, 7, 2, 3, 5, 6, 2, 3, 1, 9, 10, 15, 13, 12, 17};
 
-  printArray("Original Array a: ", a);
+  int numberOfElements;
 
-  merge_sort(a, 0, ARRAY_LENGTH - 1);
+  numberOfElements = sizeof(A)/sizeof(A[0]);
 
-  printArray("\n\nsorted a: ", a);
+  printArray("Original Array: ", A, numberOfElements);
+
+  MergeSort(A,numberOfElements);
+
+  printArray("sorted Array: ", A, numberOfElements);
 
   return 0;
 }
