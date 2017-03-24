f47229a72add0f5b0ad0f1240b0707e02b987d35
Kenner Kliemann <kenner.hp@gmail.com>
2017-03-24 20:11:26 -0300

    heap sort

--- algorithms/sorting/heapsort.c
+++ algorithms/sorting/heapsort.c
@@ -1,68 +1,87 @@
 #include <stdio.h>
 
-#define ARRAY_LENGTH 10
-#define TM_PRINTF(f_, ...) printf((f_), __VA_ARGS__)
+#define LENGTH 20
+#define TM_PRINTF(f_, ...)  printf((f_), __VA_ARGS__)
+#define PARENT(index)       ((index - 1) / 2)
+#define LEFT(index)         ((2 *index) + 1)
+#define RIGHT(index)        ((2 *index) + 2)
+
+int heapSize;
+
+// void SWAP(int *v, int i, int j) {
+//   int x = v[i];
+//   v[i] = v[j];
+//   v[j] = x;
+// }
+
+// void sobe(int *h, int tam) {
+//   int p, i = tam - 1;
+
+//   while ( h[i] > h[ p = PARENT(i) ]) {
+//     SWAP(h, i, p);
+//     i = p;
+//   }
+// }
+
+// void desce(int *h, int tam) {
+//   int i,e,d;
+
+//   i = 0;
+
+//   while ( h[i] > h[e = LEFT(i)] ) {
+//     SWAP(h, i, e);
+//     i = e;
+//   }
+// }
+
+// void remove_da_heap(int *h, int *tam) {
+//   int x;
+
+//   if (!(*tam)) return;
+
+//   // arrumar ponteiros
+//   x = h[0];
+//   h[0] = h[--(*tam)];
+
+//   desce(h, (*tam));
+// }
 
 
-int pai(int i) {
-  return (i-1)/2;
+// void insere_heap(int *h, int *tam, int x) {
+//   //teste se tem espaço e insere no final
+//   if (tam < LENGTH) h[(*tam)++] = x;
+
+//   sobe(h, tam);
+// }
+
+void Heapify(int* A, int i) {
+  int l = 2 * i + 1;
+  int r = 2 * i + 2;
+  int largest;
+
+  if(l <= heapSize && A[l] > A[i]) largest = l;
+  else largest = i;
+
+  if(r <= heapSize && A[r] > A[largest]) largest = r;
+
+  TM_PRINTF("HEAPING heapSize[%d] l[%d] r[%d] largest[%d] \n", heapSize, r, l, largest);
+
+  if(largest != i) {
+      int temp = A[i];
+      A[i] = A[largest];
+      A[largest] = temp;
+
+      Heapify(A, largest);
+    }
 }
 
-int esq(int i) {
-  return 2*i + 1;
+void BuildHeap(int* A) {
+  heapSize = LENGTH - 1;
+  int i;
+
+  for(i = (LENGTH - 1) / 2; i >= 0; i--)
+    Heapify(A, i);
 }
-
-int dir(int i) {
-  return 2*i + 2;
-}
-
-void troca(int *h, int i, int j) {
-  int swap = h[i];
-  h[i] = h[j];
-  h[j] = swap;
-}
-
-void sobe(int *h, int tam) {
-  int p, i = tam - 1;
-
-  while ( h[i] > h[ p = pai(i) ]) {
-    troca(h, i, p);
-    i = p;
-  }
-}
-
-void desce(int *h, int tam) {
-  int i,e,d;
-
-  i = 0;
-
-  while ( h[i] > h[e = esq(i)] ) {
-
-  }
-}
-
-void remove_da_heap(int *h, int tam) {
-  int x;
-
-  if (!tam) return -1;
-
-  // arrumar ponteiros
-  x = h[0];
-  h[0] = h[--tam];
-
-  desce(h, tam);
-}
-
-
-void insere_heap(int *h, int tam, int x) {
-  //teste se tem espaço e insere no final
-  if (tam < ARRAY_LENGTH) h[tam++] = x;
-
-  sobe(h, tam);
-}
-
-
-
 
 
 void printArray(char *str, int* array)  {
@@ -70,7 +89,7 @@ void printArray(char *str, int* array)  {
 
   if (str != NULL) TM_PRINTF("%s", str);
 
-  for (i = 0; i < ARRAY_LENGTH; i++) {
+  for (i = 0; i < LENGTH; i++) {
     TM_PRINTF("%d ", array[i]);
   }
 
@@ -79,9 +98,28 @@ void printArray(char *str, int* array)  {
 
 int main() {
 
-  int a[ARRAY_LENGTH] = {0};
+  int i;
+  int array[LENGTH] = { 19, 18, 17, 16, 15, 14, 13, 12, 11, 20 ,1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
 
-  printArray("Original Array a: ", a);
+  printArray("Original Array a: ", array);
+
+  heapSize = 0;
+  BuildHeap(array);
+
+  TM_PRINTF("HEAP SIZE: %d\n", heapSize);
+
+  printArray("full heap: ", array);
+
+  for(i = LENGTH - 1; i > 0; i--) {
+      int temp = array[heapSize];
+      array[heapSize] = array[0];
+      array[0] = temp;
+      heapSize--;
+      Heapify(array, 0);
+  }
+
+  printArray("sorted array: ", array);
 
   return 0;
 }
+
